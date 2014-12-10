afd52646019bdf03e9ef57e2c3cc62ea9577100e
Rajiv <rajiv.v2013@teachforindia.org>
2014-12-10 22:38:38 +0530

    bug fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,10 @@ function displayBooks($scope,$http){
   $http.get("https://arcane-forest-5176.herokuapp.com/getbooks")
   .success(function(response) {
   $scope.books = response;
-  console.log(response);
+    
+  //jsonstr =  '[{"title" : "adasdas", "description" : "asdasdas", "author": "asdasdasd"}, {"title" : "fghgfh", "description" : "gfhgfh", "author" : "fghdg"}]'; 
+  //obj = JSON.parse(jsonstr);
+  //$scope.books = obj;
+  //console.log(obj);
   });
 }
