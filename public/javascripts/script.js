632d0f1124b31bbae572b5255cee4d12c024bce2
Rajiv <rajiv.v2013@teachforindia.org>
2014-12-13 11:02:30 +0530

    Bug fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,28 +2,23 @@ var app = angular.module('books-list-app',[]);
 
 app.controller('books-list-controller',function($scope,$http){  
   var refreshBooks = function(){
-    $http.get('http://localhost:3000/getbooks').success(function(response){
+  //http://localhost:3000/getbooks
+  //https://arcane-forest-5176.herokuapp.com/getbooks
+    $http.get('https://arcane-forest-5176.herokuapp.com/getbooks').success(function(response){
       $scope.books = response;
-      console.log('here');
       console.log(response);
     }).error(function(response){
       console.log(response);
     });
-  /*
-    $http.get('https://arcane-forest-5176.herokuapp.com/getbooks').success(function(response) {
-      $scope.books = response;
-      console.log(response);
-    });
-    */
   };
   refreshBooks();
   $scope.newtitle = 'Title';
   $scope.newauthor = 'Author';
   $scope.newdescription = 'Description';
-  //
+  //http://localhost:3000/addbooks
   //https://arcane-forest-5176.herokuapp.com/addbooks
   $scope.pushBook = function(){
-    $http.post('http://localhost:3000/addbooks',{
+    $http.post('https://arcane-forest-5176.herokuapp.com/addbooks',{
       title : $scope.newtitle,
       author : $scope.newauthor,
       description : $scope.newdescription
